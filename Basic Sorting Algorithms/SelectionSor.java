eb58bbcec610cd7c63c90cba3cee6eb46003877a
developermickey <mukesh.quazma@gmail.com>
2024-10-11 17:32:34 +0530

    SelectionSort Added

---
+++ Basic Sorting Algorithms/SelectionSor.java
@@ -0,0 +1,31 @@
+public class SelectionSor {
+
+    public static void SelectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // Swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+
+    public static void printA(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 1, 3, 2 };
+        SelectSort(arr);
+        printA(arr);
+
+    }
+}
